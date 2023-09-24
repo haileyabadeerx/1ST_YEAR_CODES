27fea559a33d3d7249d5f9aad5d5300a31a630f4
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-09-24 11:57:20 +0800

    changed c file name

---
+++ unit 3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello\n");
+    printf("The value of %5d is five.",5);
+    printf("\n\n Do you know the next number? \n");
+    printf("\n %5d %6.2f", 6 ,6.5);
+    printf("'\Then followed by %d.", 7);
+
+    int a = 1;
+    printf("%d", a);
+
+    return 0;
+}
