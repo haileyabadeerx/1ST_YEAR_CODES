3cb6b8a20b3bb5bf3d63e7734a7840cef60535ec
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-09-28 00:37:17 +0800

    moved 3 versions folder

---
+++ 2D array group act/3 versions/v1/BubbleSortWhole.c
@@ -0,0 +1,25 @@
+// Sorts the entire 2D array using bubble sort in ascending or descending order
+
+#define ROWS 3
+#define COLS 3
+
+void BubbleSortWhole(double **numbers, int ifAscend) {
+    int totalElements = ROWS * COLS;
+
+    for (int i = 0; i < totalElements; i++) {
+        for (int j = 0; j < totalElements - i - 1; j++) {
+            // Calculate the row and column indices for the current and next elements
+            double *current = numbers[j / COLS] + j % COLS;
+            double *next = numbers [(j + 1) / COLS] + (j + 1) % COLS;
+
+            // Check the sorting order and compare adjacent elements
+            if ((*current > *next && ifAscend == 1) || (*current < *next && ifAscend == 2)) {
+                // Swap the elements if they are in the wrong order
+                double temp = *current;
+                *current = *next;
+                *next = temp;
+            }
+        }
+    }
+
+}
