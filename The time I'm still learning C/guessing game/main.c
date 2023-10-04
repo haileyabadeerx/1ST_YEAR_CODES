b20ea114251e874644a779c053d57c89423c0a02
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-05 00:50:12 +0800

    changed if condition

--- The time I'm still learning C/guessing game/main.c
+++ The time I'm still learning C/guessing game/main.c
@@ -14,7 +14,7 @@ int main(){
         scanf("%d", &guess);
         guessCount++;
 
-        if(guessCount < guessLimit){
+        if(guessCount < guessLimit || NoMoreGuesses > 0){
         printf("\nOopsie ! Wrong guess. Now try again.\n");
         }
         else {
