c8cbae5e2d85ed7718413a765b768b45343de33d
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-09 00:31:44 +0800

    delete

---
+++ for loops/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   /* int i = 1;
+    while (i <= 5){
+        printf("%d\n", i);
+        i++;
+    }
+*/
+
+    int motmotNumbers[]= {1, 8, 13, 20, 22};
+
+    int v;
+    for(v = 0; v < 5; v++){
+        printf("%d\n", motmotNumbers[v]);
+    }
+    return 0;
+}
---
+++ kuki/main.c
@@ -0,0 +1,239 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<dos.h>
+
+int main()
+{
+    /*
+    char CHAR_VAR='A';
+    int JETT=3;
+    int Y=1;
+    int Z=2;
+
+    switch(CHAR_VAR){
+    case 'A':
+    case 'a':
+        JETT++;
+        break;
+    case 'B':
+    case 'b':
+        JETT=Z-Y;
+        break;
+    case 'C':
+    case 'c':
+        JETT=Y-Z;
+        break;
+    default:
+        printf("INVALID ENTRY");
+    }
+    printf("%d",JETT);
+    /*
+    int ME=70;
+    int YOU=0;
+    int THEY=9;
+    int THEM=7;
+
+    switch(ME){
+    case 1:
+        YOU=ME;
+        break;
+    case 5:
+        YOU = THEY*THEM;
+        break;
+    case 2:
+        YOU++;
+        break;
+    case 7:
+        YOU-=4;
+        break;
+    default:
+        printf("%d",YOU);
+    }
+   // printf("%d",YOU);
+    /*
+    if(ME<2&&ME>0)
+        ME=YOU;
+    else if(ME==5)
+        YOU = THEY * THEM;
+    else if(ME==2)
+        YOU++;
+    else if(ME==7)
+        YOU-=4;
+    else
+        printf("%d",YOU);
+    printf("%d",YOU);
+*/
+    int a;
+    int c;
+    x: a=5;
+    c++;
+    if(c==1)
+        {a=a*a;
+         printf("The value of a is %d\n",a);
+            goto x;
+        }
+    else
+    if(c==2){
+        a=a+a;
+        printf("The value of a is %d\n",a);
+            goto x;
+    }
+    else {
+        a=a-a;
+        printf("The value of a is %d\n",a);
+    }
+    /*
+    int x;
+    float y;
+    //int y=getch();
+    printf("what?");
+    scanf("%f%5d",&y,&x);
+    printf("\n%f%d",y,x);
+    /*
+    int x=1;
+
+    if (x>0){
+        x=x+1;
+        printf("%d\n",x);
+    }
+    if(x>=1){
+        x=x+2;
+        printf("%d\n",x);
+    }
+    /*
+    int x=1;
+
+    if(x>0){
+        x=x+1;
+        printf("%d",x);
+    }
+    else if(x>=1){
+        x=x+2;
+        printf("%d",x);
+    }
+    /*
+    int x = 10; int y = 10;
+
+    while(x!=y){
+        printf("%5d %5d\n",x,y);
+    }
+    /*
+    int ctr = 0;
+
+    do
+    { ctr = ctr +1;
+      if ((ctr%2)!=0)
+        continue;
+      else
+        printf("%2d",ctr);
+    }while(ctr!=10);
+    /*
+    int i;
+    for(i=1;i<=5;i++)
+        printf("%2d",i);
+    /*
+    int x=7;int y=8;
+    if(x<=y)
+        {if(x=y)
+            x++;}
+    printf("%d",x);
+
+    /*
+    char mark='C';
+    int points;
+    /*
+    printf("Enter your score: ");
+    scanf("%c",&mark);
+    scanf("%d",&points);
+
+    switch(mark){
+    case 'A':
+        points=4;
+        break;
+        //exit();
+    case 'B':
+        points=3;
+        break;
+    case 'C':
+        points=2;
+        break;
+       // continue;
+    case 'E':
+    case 'I':
+    case 'W':
+        points=0;
+    }
+    if (points>0)
+        printf("Passed, points earned =%d\n", points);
+    else
+        printf("Failed, no points earned\n");
+    /*
+    int var1,num,z;
+
+    int k=3; int j=5;
+		if (k<4)
+			if (j>2)
+				if (k-j==3)
+			k=k+j;
+				else
+			k=k-j;
+		printf(%d\n",k);
+
+
+    /*
+    int x = 7;
+    int y = 8;
+
+    if (x<=y)
+        if(x==y)
+        x++;
+        else
+        y++;
+        printf("%d%d\"",x,y);
+
+/*
+    if (var1 > 35)
+    num=20.0;
+    else
+        if (var1 > 50)
+	  num=40.0;
+        else
+            if (var1 > 80)
+           num=60.0;
+
+    /*
+    printf("Enter a var");
+    scanf("%d",&var1);
+
+    if (var1 > 35){
+ 		    num=20.0;
+ 		    printf("%d",num);
+    }
+		else if (var1>50){
+ 		              num=40.0;
+                      printf("%d",num);
+		}
+			else {
+ 			     if(var1>80)
+ 			   num = 60.0;
+ 			   printf("%d",num);
+			}
+
+
+
+    /*
+    if (x>25.0){
+ 	   y=x;
+
+	else
+	    y=z;}
+
+
+    /*int i;
+    system("color C6");
+    for(i=1;i<=1515151515151515;i++)
+    {
+        printf(" BARBEKUKI !!! \2 \3 \t ");
+    }*/
+    return 0;
+}
